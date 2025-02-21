2129b08ff8764f45cc59c2371510722623359fea
Pawel <53155552+pmierzwinski@users.noreply.github.com>
2025-02-21 10:21:39 +0100

    copilot changes

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-import glob
 import os
+import glob
 
-STRING_MAPER = {}
+STRING_MAPPER = {}
 
-def addNewStingMapper(string):
-    if (len(STRING_MAPER) == 0):
-        STRING_MAPER[string] = 1
+def addNewStringMapper(string):
+    if len(STRING_MAPPER) == 0:
+        STRING_MAPPER[string] = 1
         return 1
-
-    last_index = max(STRING_MAPER, key=STRING_MAPER.get)
-    new_index = int(STRING_MAPER[last_index]) + 1
-    STRING_MAPER[string] = new_index
+    last_index = max(STRING_MAPPER, key=STRING_MAPPER.get)
+    new_index = int(STRING_MAPPER[last_index]) + 1
+    STRING_MAPPER[string] = new_index
     return new_index
 
 def getStringNumericIndex(string):
-    if string in STRING_MAPER:
-        return STRING_MAPER[string]
-    return addNewStingMapper(string)
+    if string in STRING_MAPPER:
+        return STRING_MAPPER[string]
+    return addNewStringMapper(string)
 
 def getFolderNames(folder_path):
     files = glob.glob(folder_path, recursive=True)
@@ -32,27 +31,21 @@ data_map = {}
 
 for folder_name in test:
     folder_data = []
-    files = glob.glob(folder_path + folder_name +"/*", recursive=True)
+    files = glob.glob(f"{folder_path}{folder_name}/*", recursive=True)
     for f in files:
         data = []
         file_path, file_extension = os.path.splitext(f)
         file_stats = os.stat(f)
-        data.append(getStringNumericIndex(
-            os.path.basename(file_path)
-        ).__str__())
-        data.append(getStringNumericIndex(
-            file_extension
-        ).__str__())
-        data.append(file_stats.st_size.__str__())
+        data.append(str(getStringNumericIndex(os.path.basename(file_path))))
+        data.append(str(getStringNumericIndex(file_extension)))
+        data.append(str(file_stats.st_size))
         folder_data.append(data)
     data_map[folder_name] = folder_data
 
-print(STRING_MAPER)
+print(STRING_MAPPER)
 
 with open('folder.data', 'w') as f:
     f.write("name,extension,size,output\n")
     for folder, data in data_map.items():
         for file_data in data:
-            f.write(','.join(file_data) + "," + folder)
-            f.write("\n")
-
+            f.write(','.join(file_data) + f",{folder}\n")
